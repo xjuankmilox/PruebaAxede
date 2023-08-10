deeb50ed79c0760917711bad2133a0d3abccfebe
xjuankmilox <juanca.gama93@gmail.com>
2023-08-10 15:11:11 -0500

    Se agrega la conexion

--- Axede/src/controller/Conexion.java
+++ Axede/src/controller/Conexion.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller;
 
 /**
@@ -9,5 +5,48 @@ package controller;
  * @author gamei
  */
 public class Conexion {
+    private String driver;
+    private String url;
+    private String usuario;
+    private String contrasena;
+    
+    public Conexion() {
+        this.driver="com.mysql.cj.jdbc.Driver";
+        this.url="jdbc:mysql://localhost:3306/";
+        this.usuario="root";
+        this.contrasena="123456";
+    }
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
     
 }
